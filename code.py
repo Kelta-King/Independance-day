6c4df0aec6afe56c53ad5835c400313653d9625f
Kelta-King <kushangshah131@gmail.com>
2022-01-26 09:42:59 +0530

    new message

--- code.py
+++ code.py
@@ -6,8 +6,8 @@ x = ["\n\033[1;34m !!! ",
     "\033[1;0m !! ", "\033[1;0m !! ", 
     "\033[1;0m !! ", "\033[1;0m !! ", "\033[1;0m !! ", 
     "\033[1;0m !! ", "\033[1;0m !! ",
-    "\n Happy Independence day!!!", 
-    " Kushang Shah salutes all the sacrifices \n made by people to give us freedom \n\n"]
+    "\n Happy Republic day!!!", 
+    " Kushang Shah salutes all the sacrifices \n made by people to make this nation great \n\n"]
 
 for item in x:
     print(item)
